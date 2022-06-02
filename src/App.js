ba1f8357a34865ceed19f277994e8d8521197477
Abdelwahab <ab28fb@gmail.com>
2022-06-02 19:34:50 +0100

    ad react icons

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Header from "./components/UI/Header";
 import Footer from "./components/UI/Footer";
 import Home from "./components/Home/Home";
@@ -15,13 +15,13 @@ import About from "./components/About/About";
 function App() {
   return (
     <>
-      {/* <h1>Application works !!</h1> */}
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="projects" element={<Projects />} />
         <Route path="about" element={<About />} />
         <Route path="resume" element={<Resume />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
     </>
