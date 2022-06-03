b533ef2c390fc4768826ffc005f84d1d99b2a575
Abdelwahab <ab28fb@gmail.com>
2022-06-03 14:38:05 +0100

    add particlesdependency

---
+++ src/components/common/Helmet.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+
+const Helmet = () => {
+    return (<>
+        <Helmet>
+            <title>Abdelwahab Portfolio</title>
+            <meta charSet="utf-8" />
+            <meta name="description" content="this is Hamadouche Abdelwahab Personal Web site a full stack developer" />
+        </Helmet>
+    </>);
+}
+export default Helmet;
