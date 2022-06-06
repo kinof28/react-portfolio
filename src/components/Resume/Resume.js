fd3f3031c40f40c9b2781e960a7069358c765d6e
Abdelwahab <ab28fb@gmail.com>
2022-06-06 14:37:30 +0100

    resume WIP

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,11 +1,80 @@
-import React from "react";
-import './Resume.css';
+import React, { useEffect, useState } from "react";
+
+// import { Page, Text, View, Document, StyleSheet } from "@react-pdf/renderer";
+
+import { Document, Page, pdfjs } from "react-pdf";
+
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import { AiOutlineDownload } from "react-icons/ai";
+
+import pdf from "../../assets/CV.pdf";
+
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+import "./Resume.css";
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+const resumeLink = "";
 
 const Resume = () => {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
   return (
-    <div>
-      <h2>Resume works !</h2>
-    </div>
+    <Container fluid className="resume-section">
+      <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Button
+          variant="primary"
+          href={pdf}
+          target="_blank"
+          style={{ maxWidth: "250px" }}
+        >
+          <AiOutlineDownload />
+          &nbsp;Download CV
+        </Button>
+      </Row>
+
+      <Row className="resume">
+        <Document file={resumeLink} className="d-flex justify-content-center">
+          <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+        </Document>
+        {/* <Document>
+          <Page size="A4" style={styles.page}>
+            <View style={styles.section}>
+              <Text>Section #1</Text>
+            </View>
+            <View style={styles.section}>
+              <Text>Section #2</Text>
+            </View>
+          </Page>
+        </Document> */}
+        {/* <Document>
+          <Page size="A4">
+            <View>
+              <Text>Section #1</Text>
+            </View>
+            <View>
+              <Text>Section #2</Text>
+            </View>
+          </Page>
+        </Document> */}
+      </Row>
+
+      <Row style={{ justifyContent: "center", position: "relative" }}>
+        <Button
+          variant="primary"
+          href={pdf}
+          target="_blank"
+          style={{ maxWidth: "250px" }}
+        >
+          <AiOutlineDownload />
+          &nbsp;Download CV
+        </Button>
+      </Row>
+    </Container>
   );
 };
 export default Resume;
