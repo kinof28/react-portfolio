8d5a3308ff270b572430d571255a8316bfb8cbe3
Abdelwahab <ab28fb@gmail.com>
2022-06-07 03:18:53 +0100

    contact responsivness

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -10,11 +10,11 @@ const Contact = () => {
         <iframe
           title="google-contact-form"
           src="https://docs.google.com/forms/d/e/1FAIpQLSeJ5I-MVffNa1rfdJBUdVRfS_zm8WdAcG0Y8jQkJTrT0a4v3g/viewform?embedded=true"
-          width="700"
-          height="920"
-          frameborder="0"
-          marginheight="0"
-          marginwidth="0"
+          //   width="700"
+          frameBorder="0"
+          marginHeight="0"
+          marginWidth="0"
+          className="i-frame"
         >
           Loading…
         </iframe>
